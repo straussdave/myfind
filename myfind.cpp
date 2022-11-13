bc5a957c6be2c9f260e66d30c53a934e88d0bfde
straussdave <davidstrauss002@gmail.com>
2022-11-13 13:01:29 +0100

    now outputs cwd, files in cwd and sets cwd to ..

--- myfind.cpp
+++ myfind.cpp
@@ -3,45 +3,60 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <errno.h>
+#include <filesystem>
+#include <iostream>
 #ifndef PATH_MAX
 #define PATH_MAX 255
 #endif
 
+using namespace std;
+
+
+int listFiles(char* currentWorkingDirectory, DIR* dirpath, dirent* direntry){
+    getcwd(currentWorkingDirectory, PATH_MAX);
+
+    printf("Current working directory: %s\n\n", currentWorkingDirectory);
+
+    if((dirpath = opendir(currentWorkingDirectory)) == NULL)
+    {
+        perror("Failed to open directory");
+        return -1;
+    }
+
+    cout << "Files in current directory: " << endl;
+
+    while((direntry = readdir(dirpath)) != NULL)
+    {
+        printf("%s ", direntry->d_name);
+        printf("%i\n", direntry->d_type); //d_type 4 == folder, d_type 8 == file
+    }
+    chdir("..");
+    return 1;
+}
+
 int main(int argc, char **argv)
 {
     char currentWorkingDirectory[PATH_MAX];
-
+    int counter = 0;
     if (getcwd(currentWorkingDirectory, PATH_MAX) == NULL)
     {
         perror("Failed to get current working directory");
         return 1;
     }
-    printf("Current working directory: %s\n", currentWorkingDirectory);
 
     struct dirent *direntry;
     DIR *dirpath;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s directory_name\n", argv[0]);
-        return 1;
-    }
-
-    if ((dirpath = opendir(argv[1])) == NULL)
+    if ((dirpath = opendir(currentWorkingDirectory)) == NULL)
     {
         perror("Failed to open directory");
-        return 1;
+        return -1;
     }
 
-    while ((direntry = readdir(dirpath)) != NULL)
-    {
-        printf("%s ", direntry->d_name);
-        printf("%i\n", direntry->d_type); //d_type 4 == folder, d_type 8 == file
-    }
-        
-    closedir(dirpath);
+    direntry = readdir(dirpath);
 
-    printf("\n");
-
+    listFiles(currentWorkingDirectory, dirpath, direntry);
+    listFiles(currentWorkingDirectory, dirpath, direntry);
+    listFiles(currentWorkingDirectory, dirpath, direntry);
     return 0;
 }

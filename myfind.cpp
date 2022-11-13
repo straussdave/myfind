f909822dae06811f77618c152db7bafec1d750d0
straussdave <davidstrauss002@gmail.com>
2022-11-13 12:30:31 +0100

    deleted gitTest, changed myfind.cpp

--- myfind.cpp
+++ myfind.cpp
@@ -1,19 +1,47 @@
 #include <limits.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <errno.h>
 #ifndef PATH_MAX
 #define PATH_MAX 255
 #endif
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
-    char mycwd[PATH_MAX];
+    char currentWorkingDirectory[PATH_MAX];
 
-    if (getcwd(mycwd, PATH_MAX) == NULL)
+    if (getcwd(currentWorkingDirectory, PATH_MAX) == NULL)
     {
         perror("Failed to get current working directory");
         return 1;
     }
-    printf("Current working directory: %s\n", mycwd);
+    printf("Current working directory: %s\n", currentWorkingDirectory);
+
+    struct dirent *direntry;
+    DIR *dirpath;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s directory_name\n", argv[0]);
+        return 1;
+    }
+
+    if ((dirpath = opendir(argv[1])) == NULL)
+    {
+        perror("Failed to open directory");
+        return 1;
+    }
+
+    while ((direntry = readdir(dirpath)) != NULL)
+    {
+        printf("%s ", direntry->d_name);
+        printf("%i\n", direntry->d_type); //d_type 4 == folder, d_type 8 == file
+    }
+        
+    closedir(dirpath);
+
+    printf("\n");
+
     return 0;
 }
